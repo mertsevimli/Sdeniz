a1e5880f42c8152437923646c102fccd17c90d94
mertsevimli <136606862+mertsevimli@users.noreply.github.com>
2024-10-10 00:02:43 +0300

    10 Ekim Proje Sdeniz

---
+++ Sdeniz/Areas/Admin/Controllers/CategoriesController.cs
@@ -0,0 +1,197 @@
+using System;
+using System.Collections.Generic;
+using System.IO; // Dosya işlemleri için eklenmeli
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http; // IFormFile kullanımı için gerekli
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Sdeniz.Data;
+using Sdeniz.Entities;
+
+namespace Sdeniz.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoriesController : Controller
+    {
+        private readonly DataBaseContext _context;
+
+        public CategoriesController(DataBaseContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/Categories
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Categories.ToListAsync());
+        }
+
+        // GET: Admin/Categories/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var category = await _context.Categories
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        // GET: Admin/Categories/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Category category, IFormFile? Image)
+        {
+            if (ModelState.IsValid)
+            {
+                if (Image != null)
+                {
+                    // Dosyanın yükleneceği dizini oluşturma
+                    string klasor = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "Img");
+                    
+                    // Klasör yoksa oluştur
+                    if (!Directory.Exists(klasor))
+                    {
+                        Directory.CreateDirectory(klasor);
+                    }
+
+                    // Dosyanın kaydedileceği tam yol
+                    string dosyaYolu = Path.Combine(klasor, Image.FileName);
+
+                    // Asenkron olarak dosyayı kaydet
+                    using var stream = new FileStream(dosyaYolu, FileMode.Create);
+                    await Image.CopyToAsync(stream);
+
+                    // Veritabanına dosya adını kaydet
+                    category.Image = Image.FileName;
+                }
+
+                _context.Add(category);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+        }
+
+        // GET: Admin/Categories/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var category = await _context.Categories.FindAsync(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        // POST: Admin/Categories/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id,  Category category,IFormFile? Image)
+        {
+            if (id != category.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    if (Image != null)
+                    {
+                        // Dosyanın yükleneceği dizini oluşturma
+                        string klasor = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "Img");
+                    
+                        // Klasör yoksa oluştur
+                        if (!Directory.Exists(klasor))
+                        {
+                            Directory.CreateDirectory(klasor);
+                        }
+
+                        // Dosyanın kaydedileceği tam yol
+                        string dosyaYolu = Path.Combine(klasor, Image.FileName);
+
+                        // Asenkron olarak dosyayı kaydet
+                        using var stream = new FileStream(dosyaYolu, FileMode.Create);
+                        await Image.CopyToAsync(stream);
+
+                        // Veritabanına dosya adını kaydet
+                        category.Image = Image.FileName;
+                    }
+                    _context.Update(category);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CategoryExists(category.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+        }
+
+        // GET: Admin/Categories/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var category = await _context.Categories
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        // POST: Admin/Categories/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var category = await _context.Categories.FindAsync(id);
+            if (category != null)
+            {
+                _context.Categories.Remove(category);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CategoryExists(int id)
+        {
+            return _context.Categories.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Sdeniz/Areas/Admin/Controllers/PostController.cs
@@ -0,0 +1,195 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Sdeniz.Data;
+using Sdeniz.Entities;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+
+namespace Sdeniz.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class PostController : Controller
+    {
+        private readonly DataBaseContext _context;
+
+        public PostController(DataBaseContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/Post
+        public async Task<IActionResult> Index()
+        {
+            var dataBaseContext = _context.Posts.Include(p => p.Category);
+            return View(await dataBaseContext.ToListAsync());
+        }
+
+        // GET: Admin/Post/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var post = await _context.Posts
+                .Include(p => p.Category)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+
+            return View(post);
+        }
+
+        // GET: Admin/Post/Create
+        public IActionResult Create()
+        {
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Name");
+            return View();
+        }
+
+        // POST: Admin/Post/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Post post, IFormFile? Image)
+        {
+            if (ModelState.IsValid)
+            {
+                // Eğer dosya varsa, dosya yüklemesini gerçekleştirin
+                if (Image != null && Image.Length > 0)
+                {
+                    // Dosya adını ve yolunu belirle
+                    var fileName = Path.GetFileName(Image.FileName);
+                    var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/Img", Image.FileName);
+
+                    // Dosyayı belirlenen yere kaydet
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await Image.CopyToAsync(stream);
+                    }
+
+                    // Post nesnesine dosya adını kaydet
+                    post.Image = fileName;
+                }
+
+                _context.Add(post);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Name", post.CategoryId);
+            return View(post);
+        }
+
+        // GET: Admin/Post/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var post = await _context.Posts.FindAsync(id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Name", post.CategoryId);
+            return View(post);
+        }
+
+        // POST: Admin/Post/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Post post, IFormFile? Image)
+        {
+            if (id != post.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    // Eğer yeni bir dosya yüklendiyse, dosya yüklemesini gerçekleştir
+                    if (Image != null && Image.Length > 0)
+                    {
+                        var fileName = Path.GetFileName(Image.FileName);
+                        var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/Img",Image.FileName);
+
+                        using (var stream = new FileStream(filePath, FileMode.Create))
+                        {
+                            await Image.CopyToAsync(stream);
+                        }
+
+                        // Eski resmin üzerine yeni resmi kaydet
+                        post.Image = fileName;
+                    }
+
+                    _context.Update(post);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PostExists(post.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Name", post.CategoryId);
+            return View(post);
+        }
+
+        // GET: Admin/Post/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var post = await _context.Posts
+                .Include(p => p.Category)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+
+            return View(post);
+        }
+
+        // POST: Admin/Post/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var post = await _context.Posts.FindAsync(id);
+            if (post != null)
+            {
+                _context.Posts.Remove(post);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PostExists(int id)
+        {
+            return _context.Posts.Any(e => e.Id == id);
+        }
+    }
+}
